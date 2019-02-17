44dfcb40a37b84a9782db5d4c4c3fd2f8c00bc83
Teri Eyenike <teyenike1@gmail.com>
2019-02-17 06:01:47 +0100

    meal routes for app

--- api/index.js
+++ api/index.js
@@ -1,12 +1,21 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
+
+// routers 
+import mealRoutes from './routes/meal.route';
 
 const app = express();
 const PORT = 7001;
+
+app.use(bodyParser.json());  
 
 app.get('/', (req, res) => {
   return res.send('The API is working, let\'s crack some code');
 })
 
+
+app.use('api/v1/meals', mealRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is currently running on PORT ${PORT}`)
