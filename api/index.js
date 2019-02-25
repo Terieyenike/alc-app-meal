2d07f658f632372843d881652f19da5ba3e00422
Teri Eyenike <teyenike1@gmail.com>
2019-02-25 09:37:13 +0100

    Travis CI and testing with mocha

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ import menuRoutes from './routes/menu.route'
 import orderRoutes from './routes/order.route'
 
 const app = express()
-const PORT = process.env.PORT || 8001
+const PORT = process.env.PORT || 9001
 
 app.use(bodyParser.json())
 
@@ -23,3 +23,5 @@ app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server is running on PORT ${PORT}, okay`)
 })
+
+export default app
