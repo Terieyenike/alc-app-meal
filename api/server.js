fda185eb0015c90af5441cdafc4d3ff5a964dde6
Teri Eyenike <teyenike1@gmail.com>
2019-02-27 14:32:32 +0100

    connect the server with the api/v1

--- api/server.js
+++ api/server.js
@@ -1,21 +1,32 @@
-var express = require('express')
-var app = express()
+import bodyParser from 'body-parser'
+import express from 'express'
+
+import mealRoutes from '.routes/meal.route'
+import menuRoutes from '.routes/menu.route'
+import orderRoutes from '.routes/order.route'
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080
 
 // set the view engine to ejs
+var app = express()
+var port = process.env.PORT || 8080
+
 app.set('view engine', 'ejs')
 
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.json())
 
 // set the home page route
 app.get('/', function(req, res) {
   // ejs render automatically looks in the views folder
   res.render('indice')
 })
+
+app.use('api/v1/meals', mealRoutes)
+app.use('api/v1/menus', menuRoutes)
+app.use('api/v1/order', orderRoutes)
 
 app.listen(port, function() {
   console.log('Our app is running on http://localhost:' + port)
