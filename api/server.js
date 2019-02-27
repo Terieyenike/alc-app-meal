9d179fa5261cc9718a522e5a81be94aa02192bef
Teri Eyenike <teyenike1@gmail.com>
2019-02-27 15:48:36 +0100

    deleted cloned repository

--- api/server.js
+++ api/server.js
@@ -1,9 +1,4 @@
-import bodyParser from 'body-parser'
-import express from 'express'
-
-import mealRoutes from '.routes/meal.route'
-import menuRoutes from '.routes/menu.route'
-import orderRoutes from '.routes/order.route'
+var express = require('express')
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
@@ -16,17 +11,12 @@ app.set('view engine', 'ejs')
 
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'))
-app.use(bodyParser.json())
 
 // set the home page route
 app.get('/', function(req, res) {
   // ejs render automatically looks in the views folder
   res.render('indice')
 })
-
-app.use('api/v1/meals', mealRoutes)
-app.use('api/v1/menus', menuRoutes)
-app.use('api/v1/order', orderRoutes)
 
 app.listen(port, function() {
   console.log('Our app is running on http://localhost:' + port)
